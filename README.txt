ae154680620748873196841d58f4ad38c616ad58
Shiva N <shiva.narayanaswamy@gmail.com>
2015-04-22 15:08:49 +1000

    HelloWorld app built using maven to be deployed to tomcat

--- README.txt
+++ README.txt
@@ -1,1 +1,19 @@
-Test Repo to run a HelloWorld app through the CodeX services
+A simple HelloWorld app to be deployed to tomcat. Uses maven for build.
+
+http://grahamlyons.com/article/hello-world-war-using-tomcat-and-maven-on-ubuntu
+
+
+
+ * In the hello directory in the workspace run the following commands: - mvn package
+
+This compiles the Java classes and puts them into a WAR file.
+
+ * sudo cp target/hello.war /var/lib/tomcat7/webapps/
+ * curl -D - http://127.0.0.1:8080/hello/
+HTTP/1.1 200 OK
+Server: Apache-Coyote/1.1
+Content-Length: 11
+Date: Tue, 18 Jun 2013 07:12:13 GMT
+
+Hello World
+
---
+++ hello/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,21 @@
+// Reflecting the directory structure where the file lives
+package org.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException
+    {
+         // Very simple - just return some plain text
+         PrintWriter writer = response.getWriter();
+         writer.print("Hello World");
+     }
+}
