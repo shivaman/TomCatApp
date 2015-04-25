9e63a28074167144a4ec6bab61f88f6877510c0b
Shiva N <shiva.narayanaswamy@gmail.com>
2015-04-25 13:58:53 +1000

    Changed to Hello Swinburne

--- hello/src/main/java/org/example/HelloServlet.java
+++ hello/src/main/java/org/example/HelloServlet.java
@@ -16,6 +16,6 @@ public class HelloServlet extends HttpServlet {
     {
          // Very simple - just return some plain text
          PrintWriter writer = response.getWriter();
-         writer.print("Hello CodeXDemo");
+         writer.print("Hello Swinburne");
      }
 }
