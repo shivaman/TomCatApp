c55cbafa131bfe9d64f47a85cbd7305f31cd718e
Shiva N <shiva.narayanaswamy@gmail.com>
2015-05-11 12:54:12 +1000

    Universe instead of World

--- hello/src/main/java/org/example/HelloServlet.java
+++ hello/src/main/java/org/example/HelloServlet.java
@@ -20,7 +20,7 @@ public class HelloServlet extends HttpServlet {
          writer.print("<HTML>");
          writer.print("<BODY>");
          writer.print("<H2>");
-         writer.print("Hello World...");
+         writer.print("Hello Universe...");
          //writer.print("Its raining bits...");
          writer.print("</H2>");
          writer.print(" <IMG SRC=\"http://www.cirrusinsight.com/wp-content/uploads/2013/03/cloud3.gif\">");
