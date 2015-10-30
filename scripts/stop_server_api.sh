9e5f5a3a88d5cbe22e282e58da1336660e1d554d
Daniel Zoltak <daniel@zoltak.com>
2015-10-30 13:15:04 +1100

    Fixed deployment bug

---
+++ scripts/stop_server_api.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Stop web server
+tomcat_running=`pgrep tomcat`
+if [[ -n  $tomcat_running ]]; then
+    service tomcat8 stop
+fi
+
+#httpd_running=`pgrep httpd`
+#if [[ -n  $httpd_running ]]; then
+#    service httpd stop
+#fi
+
+# Remove configs for replacement
+#rm -f /etc/httpd/conf/httpd.conf
+#rm -f /usr/share/tomcat8/conf/server.xml
+
+# Remove existing html pages
+rm -rf /usr/share/tomcat8/webapps/employee*
--- scripts/stop_server_web.sh
+++ scripts/stop_server_web.sh
@@ -6,15 +6,15 @@ if [[ -n  $tomcat_running ]]; then
     service tomcat8 stop
 fi
 
-httpd_running=`pgrep httpd`
-if [[ -n  $httpd_running ]]; then
-    service httpd stop
-fi
+#httpd_running=`pgrep httpd`
+#if [[ -n  $httpd_running ]]; then
+#    service httpd stop
+#fi
 
 # Remove configs for replacement
-rm -f /etc/httpd/conf/httpd.conf
-rm -f /usr/share/tomcat8/conf/server.xml
+#rm -f /etc/httpd/conf/httpd.conf
+#rm -f /usr/share/tomcat8/conf/server.xml
 
 # Remove existing html pages
 rm -rf /usr/share/tomcat8/webapps/hello.war /usr/share/tomcat8/webapps/hello*
-rm -rf /usr/share/tomcat8/webapps/api/employee*
+#rm -rf /usr/share/tomcat8/webapps/api/employee*
