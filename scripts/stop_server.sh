a1db18e5f21bbbc688a242ba7b5ebc77ba18fa1e
Daniel Zoltak <daniel@zoltak.com>
2015-10-30 12:08:51 +1100

    Added removal of server configs that will be updated

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -11,6 +11,10 @@ if [[ -n  $httpd_running ]]; then
     service httpd stop
 fi
 
+# Remove configs for replacement
+rm -f /etc/httpd/conf/httpd.conf
+rm -f /usr/share/tomcat8/conf/server.xml
+
 # Remove existing html pages
 rm -rf /usr/share/tomcat8/webapps/hello.war /usr/share/tomcat8/webapps/hello*
 rm -rf /usr/share/tomcat8/webapps/hello.war /usr/share/tomcat8/webapps/api/employee*
